3149cac32e19c138097b6afdda309c978d20bb3f
Segun Haruna <segunharuna@gmail.com>
2025-08-10 13:35:15 +0000

    added html and css

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, flash
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello, World!"
+    return render_template("home.html")
 
 
 
